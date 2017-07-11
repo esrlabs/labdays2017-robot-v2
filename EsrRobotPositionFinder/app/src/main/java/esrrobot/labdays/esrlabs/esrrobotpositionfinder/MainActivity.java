370a1fa784339cb16624f42d854af8da429f50cd
Morten Mey <morten.mey@esrlabs.com>
2017-07-11 15:59:17 +0200

    Add detection of and parsing of qr codes

--- EsrRobotPositionFinder/app/src/main/java/esrrobot/labdays/esrlabs/esrrobotpositionfinder/MainActivity.java
+++ EsrRobotPositionFinder/app/src/main/java/esrrobot/labdays/esrlabs/esrrobotpositionfinder/MainActivity.java
@@ -1,47 +1,148 @@
 package esrrobot.labdays.esrlabs.esrrobotpositionfinder;
 
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
 
-    private static final String LOG_TAG = "EsrRobotPositionFinder";
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
+    private static final String TAG = "EsrRobotPositionFinder";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private boolean mIsJavaCamera = true;
+    private MenuItem mItemSwitchCamera = null;
+    private BarcodeDetector mBarcodeDetector = null;
+    private Handler mHandler;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
     }
 
-    public MainActivity(){
-        Log.d(LOG_TAG, "Initializing opencv");
-        if(OpenCVLoader.initDebug()){
-            Log.d(LOG_TAG, "Initialized opencv");
-        }else{
-            Log.w(LOG_TAG, "Cannot initialize opencv");
-        }
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        mHandler = new Handler();
+        mBarcodeDetector = new BarcodeDetector.Builder(this).build();
 
-        System.loadLibrary("native-lib");
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.tutorial1_surface_view);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
+        }
+    }
 
-        BarcodeDetector barcodeDetector;
-        MqttClient mqttClient;
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.w(TAG, "Internal OpenCV library not found.");
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            System.loadLibrary("native-lib");
+        }
+    }
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
 
-        tv.setText(stringFromJNI());
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat mat = inputFrame.rgba();
+        Bitmap bitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(mat, bitmap);
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+        SparseArray<Barcode> barcodes = mBarcodeDetector.detect(frame);
+
+        Log.d(TAG, "Found " + barcodes.size() + " codes");
+        for (int i = 0; i < barcodes.size(); ++i) {
+            final Barcode code = barcodes.valueAt(i);
+            Log.d(TAG, "Code: " + code.rawValue);
+            for (int j = 0; j < 4; ++j) {
+                drawLine(mat, code.cornerPoints, j, (j + 1) % 4, new Scalar(255, 0, 0));
+            }
+        }
+
+        return mat;
+    }
+
+    private void drawLine(Mat mat, Point[] points, int from, int to, Scalar color) {
+        int cols = mat.cols();
+        int rows = mat.rows();
+        org.opencv.core.Point p1 = convert(points[from]);
+        org.opencv.core.Point p2 = convert(points[to]);
+        Imgproc.line(mat, p1, p2, color, 10);
+    }
+
+    private org.opencv.core.Point convert(Point p) {
+        return new org.opencv.core.Point(p.x, p.y);
     }
 
     /**
